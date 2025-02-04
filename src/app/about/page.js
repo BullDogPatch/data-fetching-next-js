8eefe868bd8fac065c7cde9a812040ebab78df96
Craig Clayton <craig__clayton@outlook.com>
2025-02-04 11:43:42 +0000

    add about route so can use metadata

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <p>this is the about me page.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,14 @@ const Header = () => {
   return (
     <header className='p-8 h-16 flex justify-between items-center'>
       <h1>This is the header</h1>
-      <Link href='/users'>Users</Link>
+      <div>
+        <Link href='/users' className='m-2 hover:underline'>
+          Users
+        </Link>
+        <Link href='/about' className='m-2 hover:underline'>
+          About
+        </Link>
+      </div>
     </header>
   );
 };
